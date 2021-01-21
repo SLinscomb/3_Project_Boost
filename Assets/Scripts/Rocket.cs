5d1592f58cb86600338d8f4fb89ffdaf05ea7f3e
SLinscomb <stephenlinscomb@gmail.com>
2021-01-21 10:26:36 -0600

    8-10

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    Rigidbody rigidBody;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ProcessInput();
+    
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.forward);
+            
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
