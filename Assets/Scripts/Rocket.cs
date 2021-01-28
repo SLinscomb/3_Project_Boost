06269d5715c8a167a99e3265310c1018438d87e4
SLinscomb <stephenlinscomb@gmail.com>
2021-01-28 10:19:08 -0600

    19. Making A Second Level
    Made level two. Changed thrust speed.

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -6,7 +6,11 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
 
+    [SerializeField] float rcsThrust = 10000f;
+    [SerializeField] float mainThrust = 1f;
+
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
 
 
@@ -14,30 +18,70 @@ public class Rocket : MonoBehaviour
 	void Start ()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        ProcessInput();
-    
-	}
+        Thrust();
+        Rotate();
+    }
 
-    private void ProcessInput()
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                // do nothing
+                break;
+            case "Fuel":
+                break;
+            default:
+                //kill player
+                break;
+        }
+    }
+
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+
+            else
+            {
+                audioSource.Stop();
+            }
+
+
         }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true;
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(Vector3.forward);
-            
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Rotate(-Vector3.forward);
         }
+
+        rigidBody.freezeRotation = false;
+
     }
+
+
 }
